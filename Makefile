d77865adc8355628cce3969c013e4a40b08f4ef8
wilkie <wilkie05@gmail.com>
2011-11-16 19:07:52 -0500

    Adding header files and linker foo.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 poi-tracker: main.cpp
-	g++ -o poi-tracker main.cpp
+	g++ -o poi-tracker main.cpp -I /usr/include/ni -I /usr/include/nite -lOpenNI -lXnVNite_1_4_2
--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+// OpenNI
+#include <ni/XnOpenNI.h>
+#include <ni/XnCppWrapper.h>
+#include <ni/XnHash.h>
+#include <ni/XnLog.h>
+
+// NITE
+#include <nite/XnVNite.h>
+
 int main(int argc, char* argv[]) {
 	return 0;
 }
